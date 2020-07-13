b6fd8d10943ce3cdd4f9a487adb3765939200815
Felipe Uzcátegui <felipe.uzcategui@gmail.com>
2020-07-12 21:19:51 -0400

    Ajuste de Actualización

---
+++ db/upgrade.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * File containing Sence Module Block class.
+ *
+ * @package    block_sence
+ * @copyright  2020 onwards Felipe Uzcátegui
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Upgrade code for the HTML block.
+ *
+ * @param int $oldversion
+ */
+function xmldb_block_sence_upgrade($oldversion) {
+    global $CFG;
+
+    return true;
+}
